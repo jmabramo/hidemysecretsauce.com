e0a1db8331a1666d88d62b2722eaf89adb9cb61d
John <jmabramo@buffalo.edu>
2022-05-12 12:52:41 -0400

    Switch to direct database access

--- api/brute_force.py
+++ api/brute_force.py
@@ -1,17 +1,17 @@
 import itertools
 import string
-import requests
+from database import users 
 import sys
+import bcrypt
+
 
 # Takes URL and Username as command line arguments.
-url = sys.argv[1]
-user = sys.argv[2]
+user = sys.argv[1]
+hash = users.find_one({'name': user})["password"]
 
-for n in range (4, 5):
+for n in range (1, 5):
     for guess in itertools.product(string.printable, repeat=n):
         guess = "".join(guess)
-        data = {"username":user, "password":guess}
-        res = requests.post(url, data = data)
-        if res.status_code == 200:
+        if bcrypt.checkpw(guess, hash):
             print(guess)
             exit()
